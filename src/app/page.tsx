81dfa4b47aa5ad8fb392954f0b49b8c903491de4
JacoboSolarte <jacobo.solarte@campusucc.edu.co>
2025-03-14 15:01:29 -0500

    Agregué la paleta de colores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-cyan-300 flex items-center justify-center">
-      <h1 className="text-cyan-900 text-4xl font-extrabold">Jacobo Solarte</h1>
+    <main className="w-screen h-screen bg-primary-foreground flex items-center justify-center">
+      <h1 className="text-accent-foreground text-4xl font-extrabold">Jacobo Solarte</h1>
     </main>
   );
 }
